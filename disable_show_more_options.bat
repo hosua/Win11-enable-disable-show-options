0cd097bc3abafeb6b1945b626d92bfd3d4457e11
hosua <hosua.o@gmail.com>
2021-12-27 06:40:57 -0500

    first commit

--- disable_show_more_options.bat
+++ disable_show_more_options.bat
@@ -4,7 +4,7 @@ reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\Inpr
 
 @echo Deleting old register
 
-cmd /c reg delete 'HKEY_CURRENT_USER\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" /f?
+cmd /c reg delete "HKEY_CURRENT_USER\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" /f?
 
 @echo Killing "explorer.exe"
 
